3c6b91fce8bfa421c5a67579000c80563a0e8268
Pierre <pierre.decrock@gmail.com>
2020-09-30 16:11:39 +0200

    delete & modify

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use App\Entity\Teams;
 use App\Form\TeamForm;
 use App\Repository\TeamsRepository;
+use Container1r6wboF\getTeamsRepositoryService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -39,7 +40,7 @@ class DefaultController extends AbstractController
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      * @var TeamsRepository $em
-     * @Route ("/home")
+     * @Route ("/home", name="home")
      */
 
     public function index(Request $request)
@@ -58,9 +59,57 @@ class DefaultController extends AbstractController
         $em = $this->entityManager->getRepository(Teams::class);
         $response = $em->findAll();
 
-        $display = $this->twig->render('Home/home.html.twig', [
+        $display = $this->twig->render('Home/blocks.html.twig', [
             'formTeam' => $form->createView(),
             'teams' => $response
+        ]);
+        return new Response($display);
+
+    }
+
+    /**
+     * @param int $id
+     * @var TeamsRepository $em
+     * @Route ("/delete/{id}", name="delete_team")
+     */
+
+    public function delete(int $id){
+
+        $em = $this->entityManager->getRepository(Teams::class);
+        $product = $em->findOneById($id);
+        $this->entityManager->remove($product);
+        $this->entityManager->flush();
+        return $this->redirectToRoute('home');
+    }
+
+    /**
+     * @param int $id
+     * @param Request $request
+     * @return Response
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     * @Route ("/modify/{id}", name="modify_team")
+     */
+    public function modify(int $id, Request $request){
+        $em = $this->entityManager->getRepository(Teams::class);
+        $team = $em->findOneById($id);
+
+
+        // Aimerais mettre l'ancien nom en placeholder de l'input
+        $form = $this->createForm(TeamForm::class, $team);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($team);
+            $this->entityManager->flush($team);
+            return $this->redirectToRoute('home');
+
+        }
+
+        $display = $this->twig->render('Home/ModifyTeam.html.twig', [
+            'formTeam' => $form->createView(),
+            'teams' => $team
         ]);
         return new Response($display);
 
